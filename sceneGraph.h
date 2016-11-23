3f1ff8d4f7912e1e14622c1b5bc0c274d7777337
Hejie Yan <hejieyan@hotmail.com>
2016-11-22 19:28:19 -0500

    Initial (unfinished) staging

---
+++ sceneGraph.h
@@ -0,0 +1,29 @@
+#ifndef SCENEGRAPH_H
+#define SCENEGRAPH_H
+
+#ifdef __APPLE__
+#  include <OpenGL/gl.h>
+#  include <OpenGL/glu.h>
+#  include <GLUT/glut.h>
+#else
+#  include <GL/gl.h>
+#  include <GL/glu.h>
+#  include <GL/freeglut.h>
+#endif
+
+// TODO: Private variables (encapsulation)
+
+class SceneGraph {
+	public:
+
+};
+
+
+
+
+
+
+
+
+
+#endif
---
+++ sceneObject.h
@@ -0,0 +1,32 @@
+#ifndef SCENEOBJECT_H
+#define SCENEOBJECT_H
+#include mathLibrary.h
+
+#ifdef __APPLE__
+#  include <OpenGL/gl.h>
+#  include <OpenGL/glu.h>
+#  include <GLUT/glut.h>
+#else
+#  include <GL/gl.h>
+#  include <GL/glu.h>
+#  include <GL/freeglut.h>
+#endif
+
+// Private variables (encapsulation)
+
+class SceneObject {
+	public:
+		Point3D objectPosition;
+		Point
+
+		SceneObject();
+
+		void 
+}
+
+
+
+
+
+
+#endif
